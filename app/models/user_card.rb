ac3ee03799298c4aaa356968a6550db366c7002d
Michael Pettitt <mikejohnpettitt@gmail.com>
2026-01-05 11:37:56 +0100

    buttons and def change

--- app/models/user_card.rb
+++ app/models/user_card.rb
@@ -2,4 +2,6 @@ class UserCard < ApplicationRecord
   belongs_to :user
   belongs_to :card
   belongs_to :preferred_definition, class_name: "Definition"
+
+  validates :card_id, uniqueness: { scope: :user_id }
 end
